d4a843f0f913ceff61e93a1f2beba04e13c36a12
JakubGaluszka96 <jakub.galuszka@interia.pl>
2022-08-04 21:50:19 +0200

    File read init

--- Elephants.py
+++ Elephants.py
@@ -1,26 +1,26 @@
 import pandas as pd
 import os
 
+test_directory="/testdata/zadanie_B/slo1.in"
+
 def readfile(file_dir):
     path = os.getcwd()+file_dir
     print(path)
     txt_file = open(path, "r")
     content = txt_file.read()
     txt_file.close()
-    return content;
+    return content
 
-#print(readfile("/testdata/zadanie_B/slo1.in"))
+
 def DataAssignment(file_dir):
     content=readfile(file_dir)
     lists=content.split('\n')
-    El_count=lists[0]
-    El_mass=lists[1].split(' ')
-    El_primary=lists[2].split(' ')
-    El_desired=lists[3].split(' ')
-    return El_count, El_mass, El_primary, El_desired;
+    elephants_count=lists[0]
+    elephants_masses=lists[1].split(' ')
+    current_order=lists[2].split(' ')
+    desired_order=lists[3].split(' ')
+    return elephants_count, elephants_masses, current_order, desired_order
 
-a, b, c, d = DataAssignment("/testdata/zadanie_B/slo1.in")
-print(a)
-print(b)
-print(c)
-print(d)
+
+elephants_count, elephants_masses, current_order, desired_order = DataAssignment(test_directory)
+print(elephants_masses)
