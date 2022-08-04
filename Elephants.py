8ef47a715ca496c0550f23308979daa2272441fc
JakubGaluszka96 <jakub.galuszka@interia.pl>
2022-08-04 23:08:50 +0200

    Method1 and 2 added

--- Elephants.py
+++ Elephants.py
@@ -15,12 +15,26 @@ def readfile(file_dir):
 def DataAssignment(file_dir):
     content=readfile(file_dir)
     lists=content.split('\n')
-    elephants_count=lists[0]
-    elephants_masses=lists[1].split(' ')
+    elephants_count=int(lists[0])
+    elephants_masses=list(map(int, lists[1].split(' ')))
     current_order=lists[2].split(' ')
     desired_order=lists[3].split(' ')
     return elephants_count, elephants_masses, current_order, desired_order
 
+def Method1():
+    elephants_count, elephants_masses, current_order, desired_order = DataAssignment(test_directory)
+    suma=sum(elephants_masses)
+    minimal=min(elephants_masses)
+    result=suma+(elephants_count-2)*minimal
+    return result
+    
+def Method2():
+    elephants_count, elephants_masses, current_order, desired_order = DataAssignment(test_directory)
+    suma=sum(elephants_masses)
+    minimal=min(elephants_masses)
+    result=suma+minimal+(elephants_count+1)*minimal
+    return result
 
 elephants_count, elephants_masses, current_order, desired_order = DataAssignment(test_directory)
-print(elephants_masses)
+print(Method1())
+print(Method2())
